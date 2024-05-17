4e76b1a30e0a28d8aa35e6987764d1aed0a14e55
Umesh Dholakiya <154407536+Umesh-Dholakiya@users.noreply.github.com>
2024-05-17 15:39:46 +0530

    add-to-cart

--- ADD- TO - CART PROJECT/assets/js/cart.js
+++ ADD- TO - CART PROJECT/assets/js/cart.js
@@ -1,43 +1,19 @@
-// quantity js 
+// // quantity js 
 
-let counter = 1;
+// let counter = 1;
 
-const decrementBtn = document.getElementById("decrement");
-const incrementBtn = document.getElementById("increment");
-const countBtn = document.getElementById("count");
+// const decrementBtn = document.getElementById("decrement");
+// const incrementBtn = document.getElementById("increment");
+// const countBtn = document.getElementById("count");
 
-incrementBtn.addEventListener("click", () => {
-    counter++;
-    countBtn.innerHTML = counter;
-});
-decrementBtn.addEventListener("click", () => {
-    if (counter > 1) {
-        counter--;
-    }
-    countBtn.innerHTML = counter;
-});
-
-
-// quantity js 
-
-let counte = 1;
-
-const decrement = document.getElementById("dec");
-const increment = document.getElementById("inc");
-const count = document.getElementById("countes");
-
-increment.addEventListener("click", () => {
-    counte++;
-    count.innerHTML = counte;
-    
-});
-decrement.addEventListener("click", () => {
-    if (counte > 1) {
-        counte--;
-    }
-    count.innerHTML = counte;
-});
-
-
-
+// incrementBtn.addEventListener("click", () => {
+//     counter++;
+//     countBtn.innerHTML = counter;
+// });
+// decrementBtn.addEventListener("click", () => {
+//     if (counter > 1) {
+//         counter--;
+//     }
+//     countBtn.innerHTML = counter;
+// });
 
